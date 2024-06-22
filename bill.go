ef2b0bd985ea0966054671bfa01e1bab39458868
kaungkhantzawdev <kaungkhantzaw.dev@gmail.com>
2024-06-22 23:36:31 +0700

    test

---
+++ bill.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func bill() {
+	fmt.Println("I am bill function.")
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	bill()
+	fmt.Println("hello I'm Go ")
+}
